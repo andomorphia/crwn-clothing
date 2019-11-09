0a1bf51dac9e8ba737bb27579deb24d5946fe139
andomorphia <andomorphia@gmail.com>
2019-11-09 17:41:49 +0100

    added shop page, collection preview and item components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
-import './directory.styles.scss'
 import MenuItem from '../menu-item/menu-item.component'
+import './directory.styles.scss'
 
 class Directory extends Component {
   constructor() {
